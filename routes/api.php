83add37f09a0d5629b7a4a75aaec2ca441d7c8f4
Lutfiyudapratama <lutfiyudapratama125@gmail.com>
2025-02-13 07:15:45 +0700

    be market

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,29 @@
 <?php
 
+use App\Http\Controllers\Authcontroller;
+use App\Http\Controllers\BannerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('product-types', ProductTypeController::class);
-Route::resource('product', ProductController::class);
+Route::post('register', [Authcontroller::class, 'register']);
+Route::post('auth', [Authcontroller::class, 'auth']);   
+
+    
+Route::group([
+    'middleware' => 'auth:sanctum'
+], function () {
+    Route::post('product', [ProductController::class, 'index']);
+    Route::post('product-update/{id}', [ProductController::class, 'update']);
+});
+
+
 Route::post('product-update/{id}', [ProductController::class, 'update']);
+Route::resource('Banner', BannerController::class);
+Route::post('product-update/{id}', [ProductController::class, 'update']);
+Route::post('banner-update/{id}', [BannerController::class, 'update']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
